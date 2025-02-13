3dc8bbd08cbf14cd698c47835800510ea492fb06
Hamadullah12 <hamadfms05@gmail.com>
2025-02-13 13:22:26 +0800

    all sections are created for frontedn ui footer and testimonials have some problems to solve

--- Ecommerece/src/pages/home/Home.jsx
+++ Ecommerece/src/pages/home/Home.jsx
@@ -1,7 +1,20 @@
+import Filter from "../../components/filter/Filter";
+import HeroSection from "../../components/herosection/HeroSection";
 import Layout from "../../components/layout/Layout";
+import ProductCard from "../../components/productcard/ProductCard";
+import Testimonial from "../../components/testimonial/Testimonial";
+import Track from "../../components/track/Track";
 
 function Home() {
-  return <Layout>Home</Layout>;
+  return (
+    <Layout>
+      <HeroSection />
+      <Filter />
+      <ProductCard />
+      <Track />
+      <Testimonial />
+    </Layout>
+  );
 }
 
 export default Home;

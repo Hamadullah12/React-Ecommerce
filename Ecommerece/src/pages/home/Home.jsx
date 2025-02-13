6c9fb655140c75a03a6c43d9eed12e4e21805397
Hamadullah12 <hamadfms05@gmail.com>
2025-02-13 12:41:08 +0800

    navbar has been created now we will be creating hero sectin

--- Ecommerece/src/pages/home/Home.jsx
+++ Ecommerece/src/pages/home/Home.jsx
@@ -1,20 +1,7 @@
-import React, { useContext } from "react";
 import Layout from "../../components/layout/Layout";
-import MyContext from "../../context/data/myContext";
 
 function Home() {
-  const context = useContext(MyContext);
-  console.log(context);
-  // const { name, rollno } = context;
-  const { state, hobby } = context;
-  return (
-    <Layout>
-      Home
-      <h1>Name:{state.name}</h1>
-      <h1>Rool Num : {state.rollno}</h1>
-      <h1>My Hobby for all day is {hobby}</h1>
-    </Layout>
-  );
+  return <Layout>Home</Layout>;
 }
 
 export default Home;
--- Ecommerece/src/pages/order/Order.jsx
+++ Ecommerece/src/pages/order/Order.jsx
@@ -1,17 +1,6 @@
-import React, { useContext } from "react";
 import Layout from "../../components/layout/Layout";
-import MyContext from "../../context/data/myContext";
 
 function Order() {
-  const context = useContext(MyContext);
-  // const { name, rollno } = context;
-  const { state } = context;
-  return (
-    <Layout>
-      Order
-      <h1>Name In Order : {state.name}</h1>
-      <h1>Roll Number In Order : {state.rollno}</h1>
-    </Layout>
-  );
+  return <Layout>Order</Layout>;
 }
 export default Order;
